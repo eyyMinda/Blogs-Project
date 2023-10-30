4ca4f8b7766a41e5eddcc59c774e23276b46a02d
eyyMinda <gamerx14x@gmail.com>
2023-10-30 03:57:51 +0200

    header to layout, dummy featured posts

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css';
+import MainNavigation from './components/layout/main-navigation';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <MainNavigation />
+
         <main>{children}</main>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,36 @@
 import { Fragment } from 'react';
 import Hero from './components/home/hero';
+import FeaturedPosts from './components/home/featured-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production it makes building fullstack React apps and sites a breeze and ships with in-built SSR.',
+    date: '2022-02-10'
+  },
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production it makes building fullstack React apps and sites a breeze and ships with in-built SSR.',
+    date: '2022-02-10'
+  },
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production it makes building fullstack React apps and sites a breeze and ships with in-built SSR.',
+    date: '2022-02-10'
+  }
+];
 
 export default function Home() {
   return (
     <Fragment>
       <Hero />
-      <section></section>
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   )
 }
