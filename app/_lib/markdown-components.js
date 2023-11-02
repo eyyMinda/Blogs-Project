b73f7ae345eb4d3e17c70d29fd4901181311ca41
eyyMinda <gamerx14x@gmail.com>
2023-11-03 00:49:46 +0200

    switched to prism, changed theme

--- app/_lib/markdown-components.js
+++ app/_lib/markdown-components.js
@@ -1,9 +1,6 @@
 import Image from "next/image";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import {
-  monokai,
-  nightOwl,
-} from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { zTouch } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 
 export const customMDComponents = {
@@ -25,7 +22,7 @@ export const customMDComponents = {
     const { className, children } = code;
     const language = className.split("-")[1];
     return (
-      <SyntaxHighlighter style={monokai} language={language}>
+      <SyntaxHighlighter style={zTouch} language={language}>
         {children}
       </SyntaxHighlighter>
     );
