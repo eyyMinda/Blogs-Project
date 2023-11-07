73cfde8ccd1b4a45f34466f6f8b98e764ffd7a48
eyyMinda <gamerx14x@gmail.com>
2023-11-07 17:43:13 +0200

    api/contact to .ts ContanctInfo interface

---
+++ app/_types/ContactType.ts
@@ -0,0 +1,5 @@
+export interface ContactInfo {
+  name: string;
+  email: string;
+  message: string;
+}
--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,8 +1,10 @@
+import { ContactInfo } from "@/app/_types/ContactType";
+
 export async function GET() {
   return Response.json({ message: "hi there" });
 }
-export async function POST(req) {
-  const data = await req.json();
+export async function POST(req: Request) {
+  const data: ContactInfo = await req.json();
   console.log("SERVER: ", data);
   return Response.json(data);
 }
