575b60dd8ff4931ed27a5008508edce7d9f8fb5e
eyyMinda <gamerx14x@gmail.com>
2023-11-09 21:47:31 +0200

    Successfully posted to MongoDb

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,16 @@
 import { ContactInfo } from "@/app/_types/ContactType";
 import { validateMultipleInputs } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
+import { connectToMongo, isMongoClient, postToMongo } from "@/lib/api-util";
+
+const getClient = async () => {
+  try {
+    const client = await connectToMongo();
+    return client;
+  } catch {
+    return NextResponse.json({ err: true, message: ["Failed to connect to the database"] }, { status: 500 });
+  }
+};
 
 export async function GET() {
   return Response.json({ message: "hi there" });
@@ -14,7 +24,15 @@ export async function POST(req: NextRequest) {
   if (errors.length > 0) return NextResponse.json({ err: true, message: errors }, { status: 422 });
 
   const newMessage = data;
-  console.log("SERVER: ", newMessage);
+
+  const client = await getClient();
+  if (!isMongoClient(client)) return client;
+
+  try {
+    await postToMongo(client, "messages", newMessage);
+  } catch (error) {
+    return NextResponse.json({ err: true, message: "Failed to post a message." }, { status: 500 });
+  }
 
   return NextResponse.json({ err: false, message: "Successfully sent a message!" }, { status: 200 });
 }
