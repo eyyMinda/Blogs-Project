4d10af73e06b52cd63e694ff452bdd8c0516d53d
eyyMinda <gamerx14x@gmail.com>
2023-11-11 01:07:23 +0200

    seperate timer for success and error

--- lib/context/notification-context.tsx
+++ lib/context/notification-context.tsx
@@ -9,16 +9,18 @@ const NotificationContext = createContext<NotificationContextType>({
 export function NotificationProvider({ children }: { children: React.ReactNode }) {
   const [notification, setNotification] = useState<NotificationData | null>(null);
 
+  // Check if status or error and set timer to remove the notification.
   useEffect(() => {
-    const statuses = ["success", "error"];
-    if (notification && statuses.includes(notification.status)) {
-      const timer = setTimeout(() => setNotification(null), 4500);
+    const statuses = { success: 4000, error: 7000 };
+
+    if (notification && notification.status in statuses) {
+      const removeTimer = statuses[notification.status as keyof typeof statuses];
+      const timer = setTimeout(() => setNotification(null), removeTimer);
       return () => clearTimeout(timer);
     }
   }, [notification]);
 
   const context = { notification, setNotification };
-
   return <NotificationContext.Provider value={context}>{children}</NotificationContext.Provider>;
 }
 
