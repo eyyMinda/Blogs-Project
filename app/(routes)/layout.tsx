7d5fd9c5c6c07f6d5dd598010b8967ca5c729e6d
eyyMinda <gamerx14x@gmail.com>
2023-11-12 22:45:06 +0200

    supressHydration for body too

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={GeistSans.className}>
+      <body className={GeistSans.className} suppressHydrationWarning={true}>
         <NotificationProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             <Header />
