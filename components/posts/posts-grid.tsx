6af1e920fb97abdb598f6206c3752b12c709f6cb
eyyMinda <gamerx14x@gmail.com>
2023-11-13 23:54:39 +0200

    Post-item replaced with shad template

--- components/posts/posts-grid.tsx
+++ components/posts/posts-grid.tsx
@@ -1,12 +1,14 @@
 import css from "./styles/posts-grid.module.css";
 import PostItem from "./post-item";
+import ShadItem from "./shad-item";
 import { Posts } from "@/app/_types/PostType";
 
 const PostsGrid: React.FC<Posts> = ({ posts }) => {
   return (
+    //   <div className="grid grid-cols-3 gap-8"></div>
     <ul className={css.grid}>
       {posts?.map(post => (
-        <PostItem key={post.slug} post={post} />
+        <ShadItem key={post.slug} post={post} />
       ))}
     </ul>
   );
