6333fdcc9497715ab894898e7e36d8e6deb8be88
eyyMinda <gamerx14x@gmail.com>
2023-11-13 05:21:36 +0200

    Login/Register page init

--- components/auth/auth-form.tsx
+++ components/auth/auth-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import classes from "./auth-form.module.css";
+import css from "./auth-form.module.css";
 
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
@@ -10,20 +10,20 @@ function AuthForm() {
   };
 
   return (
-    <section className={classes.auth}>
+    <section className={css.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form>
-        <div className={classes.control}>
+        <div className={css.control}>
           <label htmlFor="email">Your Email</label>
           <input type="email" id="email" required />
         </div>
-        <div className={classes.control}>
+        <div className={css.control}>
           <label htmlFor="password">Your Password</label>
           <input type="password" id="password" required />
         </div>
-        <div className={classes.actions}>
+        <div className={css.actions}>
           <button>{isLogin ? "Login" : "Create Account"}</button>
-          <button type="button" className={classes.toggle} onClick={switchAuthModeHandler}>
+          <button type="button" className={css.toggle} onClick={switchAuthModeHandler}>
             {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
