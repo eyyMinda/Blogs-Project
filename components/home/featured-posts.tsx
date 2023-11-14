6ed39dafddc40a89b751027f6c150845a1598ef9
eyyMinda <gamerx14x@gmail.com>
2023-11-14 17:23:59 +0200

    all-posts & featured-posts: from module.css to tailwind, added three-d-title

--- components/home/featured-posts.tsx
+++ components/home/featured-posts.tsx
@@ -1,11 +1,10 @@
-import css from "./styles/featured-posts.module.css";
 import PostsGrid from "../posts/posts-grid";
 import { Posts } from "@/app/_types/PostType";
 
 const FeaturedPosts: React.FC<Posts> = ({ posts }) => {
   return (
-    <section className={css.latest}>
-      <h1>Featured Posts</h1>
+    <section className="w-[95%] max-w-7xl mx-auto my-8">
+      <h1 className="three-d-title">Featured Posts</h1>
 
       <PostsGrid posts={posts} />
     </section>
--- components/posts/all-posts.tsx
+++ components/posts/all-posts.tsx
@@ -1,11 +1,10 @@
 import { Posts } from "@/app/_types/PostType";
 import PostsGrid from "./posts-grid";
-import css from "./styles/all-posts.module.css";
 
 const AllPosts: React.FC<Posts> = ({ posts }) => {
   return (
-    <section className={css.posts}>
-      <h1>AllPosts</h1>
+    <section className="w-[95%] max-w-7xl mx-auto my-8">
+      <h1 className="three-d-title">All Posts</h1>
       <PostsGrid posts={posts} />
     </section>
   );
