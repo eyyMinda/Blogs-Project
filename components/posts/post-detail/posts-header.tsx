cd5441e55d0eb5fa17bf00380020b52b266d881e
eyyMinda <gamerx14x@gmail.com>
2023-11-14 23:15:26 +0200

    post head to tailwind

--- components/posts/post-detail/posts-header.tsx
+++ components/posts/post-detail/posts-header.tsx
@@ -1,12 +1,11 @@
-import css from "./styles/post-header.module.css";
 import Image from "next/image";
 import { PostHeader } from "@/app/_types/PostType";
 
 const PostHeader = ({ title, image }: PostHeader) => {
   return (
-    <header className={css.header}>
-      <h1>{title}</h1>
-      <Image src={image} alt="title" width={200} height={150} />
+    <header className="flex-col-reverse sm:flex-row flex gap-4 justify-between items-center lg:items-end border-b-8 rounded-2xl border-title pb-8 mb-8">
+      <h1 className="three-d-title text-4xl lg:text-5xl min-w-[15rem]">{title}</h1>
+      <Image src={image} alt="title" width={200} height={150} className="min-w md:min-w-[20rem] max-h-40 object-cover" />
     </header>
   );
 };
