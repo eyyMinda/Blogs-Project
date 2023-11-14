e6a66b66713c075ec64d9ed79217bffb87c0e849
eyyMinda <gamerx14x@gmail.com>
2023-11-15 00:15:24 +0200

    added: alert potentially instead of notification; alert variant - success

--- components/ui/notification.tsx
+++ components/ui/notification.tsx
@@ -3,6 +3,9 @@ import { useContext } from "react";
 import NotificationContext from "@/lib/context/notification-context";
 import ReactDOM from "react-dom";
 
+import { AlertCircle } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 function Notification({ title, message, status }: NotificationData) {
   const notifCtx = useContext(NotificationContext);
   const divCss = `${css.notification} ${css[status]}`;
@@ -26,6 +29,23 @@ function Notification({ title, message, status }: NotificationData) {
         <p>{message}</p>
       )}
     </div>,
+    // (<Alert variant="success" onClick={() => notifCtx.setNotification(null)}>
+    //   <AlertCircle className="h-4 w-4" />
+    //   <AlertTitle>{title}</AlertTitle>
+    //   <AlertDescription>
+    //     {Array.isArray(message) ? (
+    //       <ul>
+    //         {message.map((m, i) => (
+    //           <li key={i}>
+    //             <p>{m}</p>
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     ) : (
+    //       <p>{message}</p>
+    //     )}
+    //   </AlertDescription>
+    // </Alert>)
     target
   );
 }
