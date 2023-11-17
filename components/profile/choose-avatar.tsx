fe28426f8f3d6e1e2636ee9b7cb6fc8727859d7f
eyyMinda <gamerx14x@gmail.com>
2023-11-17 07:38:27 +0200

    Change Avatar popover

--- components/profile/choose-avatar.tsx
+++ components/profile/choose-avatar.tsx
@@ -1,21 +1,52 @@
-import { getFolderFileNames } from "@/lib/posts-util";
-import Image from "next/image";
+"use client";
 
-export default function ChooseAvatar() {
-  const images = getFolderFileNames("public/images/account/remix-rumble-avatars");
-  const prePath = "/images/account/remix-rumble-avatars/";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { useRef, useState } from "react";
+import { PopoverClose } from "@radix-ui/react-popover";
+
+const prePath = "/images/account/remix-rumble-avatars/";
+
+export default function ChooseAvatar({ images }: { images: string[] }) {
+  const [selected, setSelected] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const popoverCloseRef = useRef<HTMLButtonElement>(null);
+
+  function handleChooseAvatar() {
+    setIsLoading(true);
+    console.log(selected);
+    // update slected avatar to db ==>> profile data
+    setTimeout(() => {
+      setIsLoading(false);
+      // Send notification
+      popoverCloseRef.current?.click();
+    }, 2000);
+  }
 
   return (
-    <section>
-      <h1>Choose Avatar</h1>
-
-      <div className="grid grid-cols-6 gap-2 m-4 border-4 p-2 max-h-[20rem] overflow-y-scroll">
-        {images?.map((img, i) => (
-          <div key={i} className="border-2 border-gray-600 hover:border-gray-400 cursor-pointer">
-            <Image src={prePath + img} alt={img} width={70} height={70} className="w-20 h-20" />
-          </div>
-        ))}
+    <section className="flex flex-col m-4">
+      <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 gap-2 mb-8 border-4 py-2 max-h-[30rem] overflow-y-scroll">
+        {images?.map((img, i) => {
+          const fullPath = prePath + img;
+          const isSelected = selected === fullPath;
+          return (
+            <div key={i} className={`relative ${isSelected ? "border-4 border-white" : "border-2 border-gray-500"} hover:border-gray-300 w-30 h-30`}>
+              <input
+                type="checkbox"
+                name={img}
+                className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                onClick={() => setSelected(prev => (prev === fullPath ? null : fullPath))}
+              />
+              <Image src={fullPath} alt={img} width={100} height={100} className="w-full h-auto" />
+            </div>
+          );
+        })}
       </div>
+
+      <Button type="button" variant="secondary" className="hover:bg-title" onClick={handleChooseAvatar} disabled={!selected}>
+        {isLoading ? "Loading..." : "Choose Avatar"}
+      </Button>
+      <PopoverClose ref={popoverCloseRef} />
     </section>
   );
 }
--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -2,20 +2,29 @@
 import ProfileForm from "./profile-form";
 import DisplayAvatar from "./display-avatar";
 import ChooseAvatar from "./choose-avatar";
+import { getFolderFileNames } from "@/lib/posts-util";
+import { Popover, PopoverTrigger, PopoverContent } from "../ui/popover";
 
 function UserProfile() {
+  const images = getFolderFileNames("public/images/account/remix-rumble-avatars");
   // Redirect away if NOT auth
   // redirect("/login");
-
+  function handlePopover() {}
   return (
     <section className="mx-auto my-12 text-center">
       <header className="flex flex-col items-center gap-3">
-        <DisplayAvatar />
+        <Popover>
+          <PopoverTrigger>
+            <DisplayAvatar />
+          </PopoverTrigger>
+
+          <PopoverContent className="w-[90vw] md:w-[40rem] -translate-y-56 md:-translate-y-72">
+            <ChooseAvatar images={images} />
+          </PopoverContent>
+        </Popover>
 
         <h1 className="text-4xl">Your User Profile</h1>
       </header>
-
-      <ChooseAvatar />
 
       <ProfileForm />
     </section>
