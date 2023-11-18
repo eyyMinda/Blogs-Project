44fff274039b225a68fa005020de0cd3606cecc0
eyyMinda <gamerx14x@gmail.com>
2023-11-18 14:06:07 +0200

    moved submit button to ui

--- components/contact/submit-btn.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "../ui/button";
-
-export default function SubmitButton({ isLoading }: { isLoading: boolean }) {
-  return (
-    <Button type="submit" aria-disabled={isLoading} disabled={isLoading}>
-      {isLoading ? "Sending..." : "Send Message"}
-    </Button>
-  );
-}
