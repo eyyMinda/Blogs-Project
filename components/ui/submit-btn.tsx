49a45b9e1b7d52fd06ddb145584cfc20166dc5b8
eyyMinda <gamerx14x@gmail.com>
2023-11-18 14:49:24 +0200

    submitbutton, can optional className

--- components/ui/submit-btn.tsx
+++ components/ui/submit-btn.tsx
@@ -5,12 +5,13 @@ type SubmitButton = {
   isLoading: boolean;
   loadingText?: string;
   text?: string;
+  className?: string;
 };
 
-export default function SubmitButton({ isLoading, loadingText, text = "Send Message" }: SubmitButton) {
+export default function SubmitButton({ isLoading, loadingText, text = "Send Message", className = "" }: SubmitButton) {
   <Loader2 className="animate-spin" />;
   return (
-    <Button type="submit" aria-disabled={isLoading} disabled={isLoading}>
+    <Button type="submit" aria-disabled={isLoading} disabled={isLoading} className={className}>
       {isLoading ? loadingText || <Loader2 className="animate-spin" /> : text}
     </Button>
   );
