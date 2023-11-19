0e0e93c2d3de3e2558b23abfc55d84301b12d5f6
eyyMinda <gamerx14x@gmail.com>
2023-11-19 18:20:58 +0200

    added verifyPassword in utils

--- lib/auth-valid/auth.ts
+++ lib/auth-valid/auth.ts
@@ -1,3 +1,5 @@
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 
 export const hashPassword = async (password: string) => hash(password, 12);
+
+export const verifyPassword = async (password: string, hashedPassword: string) => compare(password, hashedPassword);
