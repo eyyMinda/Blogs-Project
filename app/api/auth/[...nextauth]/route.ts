f2aaf47c10b0319bae0524b98018dd86398935a6
eyyMinda <gamerx14x@gmail.com>
2023-11-22 14:46:58 +0200

    attempt to add: github session verification with db based on email address

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth";
+import NextAuth, { User as AuthUser, Account } from "next-auth";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import { connectToMongo, getFromMongo, isMongoClient } from "@/lib/mongo-db/mongo";
+import { connectToMongo, getFromMongo, isMongoClient, postToMongo, updateInMongo } from "@/lib/mongo-db/mongo";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from "next-auth/providers/github";
-import { verifyPassword } from "@/lib/auth-valid/auth";
+import { hashPassword, verifyPassword } from "@/lib/auth-valid/auth";
 import { isValid } from "@/lib/auth-valid/isValid";
 
-const authOptions = {
+export const authOptions: any = {
   session: {
     maxAge: 60 * 60 * 24 * 14
   },
@@ -39,15 +39,67 @@ const authOptions = {
         const passwordMatch = await verifyPassword(data.password, matchedUser.password);
         if (passwordMatch) throw new Error("Incorrect password for this email account.");
 
-        return { id: matchedUser.id, email: matchedUser.email };
+        return { id: matchedUser.id, email: matchedUser.email, username: matchedUser.name, createdAt: matchedUser.createdAt };
       }
     }),
     GitHubProvider({
       clientId: process.env.NEXT_PUBLIC_GITHUB_ID ?? "",
       clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET ?? ""
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account }: { user: AuthUser; account: Account }) {
+      console.log("checking provider");
+      if (account.provider === "credentials") return true;
+      console.log("provider is not credentials??");
+
+      if (account.provider === "github") {
+        let client;
+        try {
+          client = await connectToMongo();
+          if (!isMongoClient(client)) throw new Error("Failed to connect to the database.");
+        } catch (error) {
+          throw new Error("Failed to connect to the database.");
+        }
+
+        const email = user.email || "";
+        const date = new Date();
+        const tempUsername = "user_" + (await hashPassword(email.substring(0, email.indexOf("@")))).slice(0, 9);
+        const signUpData = {
+          email: user.email,
+          name: tempUsername,
+          createdAt: date,
+          updatedAt: date
+        };
+        user.name = tempUsername;
+
+        const matchedUser = (await getFromMongo(client, "users", { email: email }))[0] as User;
+        if (!matchedUser) {
+          try {
+            await postToMongo(client, "users", signUpData);
+          } catch (error) {
+            throw new Error("Failed to Sign-up on the database.");
+          }
+        } else {
+          try {
+            await updateInMongo(
+              client,
+              "users",
+              { id: matchedUser.id },
+              {
+                name: tempUsername,
+                updatedAt: date
+              }
+            );
+          } catch (error) {
+            throw new Error("Failed to Update user on the database.");
+          }
+        }
+      }
+      return true;
+    }
+  }
 };
 
-const handler = NextAuth(authOptions);
+export const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
