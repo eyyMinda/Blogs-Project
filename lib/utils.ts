fbc349b5bb1935d9c9dd39ecf259fc537a69db69
eyyMinda <gamerx14x@gmail.com>
2023-11-25 23:17:46 +0200

    format date with nth

--- lib/utils.ts
+++ lib/utils.ts
@@ -40,3 +40,31 @@ export const trimObjectValues = (data: DataObject, excludeKeys: string[] = []): 
       return excludeKeys.includes(key) ? [key, value] : [key, removeBlankCharacters(value).trim()];
     })
   );
+
+export const nth = (d: number) => {
+  if (d > 3 && d < 21) return "th";
+  switch (d % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
+};
+
+export const formatDate = (dateStr: string) => {
+  const date = new Date(dateStr);
+  const day = date.getDate();
+  const nthSuffix = nth(day);
+
+  const monthYear = date.toLocaleDateString("en-US", {
+    month: "long",
+    year: "numeric"
+  });
+
+  const formattedDate = `${day + nthSuffix} ${monthYear}`;
+  return formattedDate;
+};
