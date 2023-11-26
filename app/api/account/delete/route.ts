5dfebfaedc373f3339a6565bfcb1d869e24f3697
eyyMinda <gamerx14x@gmail.com>
2023-11-26 16:53:34 +0200

    delete account functionality

---
+++ app/api/account/delete/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { deleteFromMongo, getClient, isMongoClient } from "@/lib/mongo-db/mongo";
+
+// --------------------------- POST -------------------------------------
+export async function POST(req: NextRequest) {
+  const email: DataObject = await req.json();
+  if (!email) return NextResponse.json({ err: true, msg: "No email has been provided." }, { status: 400 });
+
+  // ============= Define/Redefine Client =============================
+  const client = await getClient();
+  if (!isMongoClient(client)) return client;
+
+  // ============== Check if User is Authorized ==============================
+  const session = await getServerSession();
+  if (!session) return NextResponse.json({ err: true, msg: "You are not authorized!" }, { status: 401 });
+
+  // ================== Delete User ===================================
+  try {
+    await deleteFromMongo(client, "users", { email: email });
+  } catch (error) {
+    return NextResponse.json({ err: true, msg: "Failed to Delete account." }, { status: 500 });
+  }
+
+  return NextResponse.json({ err: false, msg: `Account has been deleted.` }, { status: 200 });
+}
--- components/profile/delete-account.tsx
+++ components/profile/delete-account.tsx
@@ -1,15 +1,24 @@
 "use client";
+import { signOut } from "next-auth/react";
+import { useContext } from "react";
+import NotificationContext from "@/lib/context/notification-context";
+import defaultNotification from "@/lib/locale/default-notification";
 import { Button } from "../ui/button";
 
 export default function DeleteAccount({ email }: { email?: string | null }) {
+  const notifCtx = useContext(NotificationContext);
   if (!email) return;
   const handleDelete = async () => {
     if (window.confirm("Ae you sure you want to delete your account?")) {
+      notifCtx.setNotification(defaultNotification["deleteacc"]["pending"]);
       const res = await fetch("/api/account/delete", {
-        method: "DELETE",
+        method: "POST",
         body: JSON.stringify(email)
       });
-      // const {err, msg} = res.json();
+      const { err, msg } = await res.json();
+
+      if (!err) signOut();
+      notifCtx.setNotification(defaultNotification["deleteacc"][err ? "error" : "success"](msg));
     } else {
       return;
     }
