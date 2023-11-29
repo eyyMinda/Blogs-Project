af6cd528c137c0f2bb36a7eebbdb25bf05f82faa
eyyMinda <gamerx14x@gmail.com>
2023-11-29 12:45:23 +0200

    added privacy policy

---
+++ app/(routes)/(required)/privacy/page.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { customMDComponents } from "@/lib/markdown-components";
+import { getPostData } from "@/lib/posts-util";
+import ReactMarkdown from "react-markdown";
+
+export default async function Privacy() {
+  const privacy = getPostData("privacy-policy", "/");
+
+  return (
+    <Card className="border-2 my-6">
+      <CardContent className="markdown-content text-lg md:px-8 max-w-5xl">
+        <ReactMarkdown components={customMDComponents}>{privacy.content}</ReactMarkdown>
+      </CardContent>
+    </Card>
+  );
+}
