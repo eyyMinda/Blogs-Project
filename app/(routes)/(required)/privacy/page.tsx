2bd114c1ca3b4db084a8567dba6caf02bd408a13
eyyMinda <gamerx14x@gmail.com>
2023-11-29 13:20:06 +0200

    replace with current date, edit for light theme

--- app/(routes)/(required)/privacy/page.tsx
+++ app/(routes)/(required)/privacy/page.tsx
@@ -5,11 +5,12 @@ import ReactMarkdown from "react-markdown";
 
 export default async function Privacy() {
   const privacy = getPostData("privacy-policy", "/");
-
+  const currentDate = new Date().toLocaleString("en-US", { year: "numeric", month: "long" });
+  const content = privacy.content.replace(/\[\[Date\]\]/g, currentDate);
   return (
     <Card className="border-2 my-6">
       <CardContent className="markdown-content text-lg md:px-8 max-w-5xl">
-        <ReactMarkdown components={customMDComponents}>{privacy.content}</ReactMarkdown>
+        <ReactMarkdown components={customMDComponents}>{content}</ReactMarkdown>
       </CardContent>
     </Card>
   );
