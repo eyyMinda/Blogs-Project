6f9cd4f4452d69e3d370f97eea1e5f1e1c0151b5
eyyMinda <gamerx14x@gmail.com>
2023-11-29 13:53:35 +0200

    accessability and seo edit

--- components/posts/post-item.tsx
+++ components/posts/post-item.tsx
@@ -14,25 +14,28 @@ const PostItem: React.FC<PostObject> = ({ post }) => {
   const imagePath = image ? `/images${linkPath}/${image}` : "/images/default-blog.webp";
 
   return (
-    <Card className="flex flex-col content-between">
-      <CardHeader className="p-0">
-        <Link href={linkPath}>
-          <Image src={imagePath} width={300} height={200} alt={title} priority className="w-[100%] object-cover h-48" />
-        </Link>
-      </CardHeader>
-      <CardContent className="text-center p-[1rem_.5rem_.5rem]">
-        <CardTitle className="pb-2">{title}</CardTitle>
-        <CardDescription className="text-md italic">
-          <time>{formattedDate}</time>
-        </CardDescription>
-        <p>{excerpt}</p>
-      </CardContent>
-      <CardFooter className="p-4 pt-0 mt-auto justify-center">
-        <Link href={linkPath} className="text-purple-600 hover:text-purple-700">
-          Read More
-        </Link>
-      </CardFooter>
-    </Card>
+    <li>
+      <Card className="flex flex-col content-between">
+        <CardHeader className="p-0">
+          <Link href={linkPath}>
+            <span className="sr-only">Read more</span>
+            <Image src={imagePath} width={300} height={200} alt={title} priority className="w-[100%] object-cover h-48" />
+          </Link>
+        </CardHeader>
+        <CardContent className="text-center p-[1rem_.5rem_.5rem]">
+          <CardTitle className="pb-2">{title}</CardTitle>
+          <CardDescription className="text-md italic">
+            <time>{formattedDate}</time>
+          </CardDescription>
+          <p>{excerpt}</p>
+        </CardContent>
+        <CardFooter className="p-4 pt-0 mt-auto justify-center">
+          <Link href={linkPath} className="text-purple-600 hover:text-purple-700">
+            Read More
+          </Link>
+        </CardFooter>
+      </Card>
+    </li>
   );
 };
 
