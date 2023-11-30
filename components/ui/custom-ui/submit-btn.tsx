70bceee6ecb2e4f730444c17cf14f71db032b537
eyyMinda <gamerx14x@gmail.com>
2023-11-30 22:02:18 +0200

    submitbutton optional variant

--- components/ui/custom-ui/submit-btn.tsx
+++ components/ui/custom-ui/submit-btn.tsx
@@ -5,13 +5,14 @@ type SubmitButton = {
   isLoading: boolean;
   loadingText?: string;
   text?: string;
+  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | null | undefined;
   className?: string;
 };
 
-export default function SubmitButton({ isLoading, loadingText, text = "Send Message", className = "" }: SubmitButton) {
+export default function SubmitButton({ isLoading, loadingText, text = "Send Message", variant = "default", className = "" }: SubmitButton) {
   <Loader2 className="animate-spin" />;
   return (
-    <Button type="submit" aria-disabled={isLoading} disabled={isLoading} className={className}>
+    <Button type="submit" variant={variant} aria-disabled={isLoading} disabled={isLoading} className={className}>
       {isLoading ? loadingText || <Loader2 className="animate-spin" /> : text}
     </Button>
   );
