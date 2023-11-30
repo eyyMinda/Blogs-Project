1ae5612a037759a3889c6e2bc2e4b2f4a81f8b1a
eyyMinda <gamerx14x@gmail.com>
2023-11-30 22:02:56 +0200

    layout for update password form, animated

--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -2,15 +2,18 @@
 import { IUser } from "@/nextauth";
 import { Popover, PopoverTrigger, PopoverContent } from "../ui/popover";
 import { timeAgo } from "@/lib/utils";
-import ProfileForm from "./profile-form";
+import NewPasswordForm from "./new-password";
 import DisplayAvatar from "./display-avatar";
 import ChooseAvatar from "./choose-avatar";
 import DeleteAccount from "./delete-account";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { Loader2 } from "lucide-react";
+import { Loader2, ShieldAlert } from "lucide-react";
+import { Button } from "../ui/button";
+import { useState } from "react";
 
 function UserProfile({ avatars }: { avatars: string[] }) {
+  const [changePassForm, setChangePassForm] = useState<boolean>(false);
   const { data: session, status } = useSession();
   const router = useRouter();
   if (status === "loading") {
@@ -23,6 +26,10 @@ function UserProfile({ avatars }: { avatars: string[] }) {
   const { image, name, email, createdAt, needPassword } = session?.user as IUser;
   let formattedDate;
   if (createdAt) formattedDate = timeAgo(createdAt);
+
+  const handleChangePassForm = () => {
+    setChangePassForm(v => !v);
+  };
 
   return (
     <section className="mx-auto my-8 px-8 text-center">
@@ -41,8 +48,26 @@ function UserProfile({ avatars }: { avatars: string[] }) {
         {formattedDate && <p className="text-gray-500">Joined {formattedDate}</p>}
       </header>
 
-      {needPassword && <h2 className="text-xl">Create a password to login without 3rd-party</h2>}
-      <ProfileForm />
+      <div className="flex flex-col items-start">
+        {needPassword && (
+          <div className="flex items-center gap-2 text-destructive text-lg">
+            <ShieldAlert />
+            Create a password to login without 3rd-party
+          </div>
+        )}
+        <hr className="w-full my-4" />
+        <div className="w-full flex items-center justify-between">
+          <Button variant="secondary" size={changePassForm ? "sm" : "default"} onClick={handleChangePassForm} className={`${changePassForm ? "order-1" : ""}`}>
+            {changePassForm ? "Cancel" : "Change Password"}
+          </Button>
+          {changePassForm && <h3 className="px-4 py-2 text-sm font-semibold bg-secondary rounded-md transition-all block btnMimick">Change Password</h3>}
+        </div>
+
+        <div className={`accordion-by-state ${changePassForm ? "open" : ""}`}>
+          <hr className="w-full my-4" />
+          <NewPasswordForm needPassword={needPassword} />
+        </div>
+      </div>
 
       <DeleteAccount email={email} />
     </section>
