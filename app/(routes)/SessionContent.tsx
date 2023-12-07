423ef6c3e36c618ef2b446651efa916cb2285112
eyyMinda <gamerx14x@gmail.com>
2023-12-07 23:37:33 +0200

    main style edit

--- app/(routes)/SessionContent.tsx
+++ app/(routes)/SessionContent.tsx
@@ -7,7 +7,7 @@ export function SessionContent({ children }: { children: React.ReactNode }) {
     <SessionProvider>
       <Header />
 
-      <main className="flex min-h-full flex-col items-center justify-between p-6 sm:px-16">{children}</main>
+      <main className="flex min-h-full max-w-[1920px] mx-auto flex-col items-center justify-between py-6 px-4 sm:px-16 lg:px-24 2xl:px-80">{children}</main>
       <div id="notifications" className="w-full h-full pointer-events-none overflow-hidden"></div>
     </SessionProvider>
   );
