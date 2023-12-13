eaf1750494a392cdbba21634fff896dfcb76d2a1
eyyMinda <gamerx14x@gmail.com>
2023-12-13 21:00:38 +0200

    edit: update condition for password & username

--- app/api/account/update/route.ts
+++ app/api/account/update/route.ts
@@ -9,35 +9,44 @@ export async function POST(req: NextRequest) {
   const data: DataObject = await req.json();
   if (!data) return NextResponse.json({ err: true, msg: "No data has been provided." }, { status: 400 });
 
-  console.log(data);
-  // ================== Validation ====================================
-  const errors = validateMultipleInputs(Object.values(data), Object.keys(data));
-  if (errors.length > 0) return NextResponse.json({ err: true, msg: errors }, { status: 422 });
-
-  const { email, password, passwordOld } = trimObjectValues(data);
+  // ============== Check if User is Authorized ==============================
+  const session = (await getServerSession()) as any;
+  if (!session) return NextResponse.json({ err: true, msg: "You are not authorized! This action will be noted." }, { status: 401 });
 
   // ============= Define/Redefine Client =============================
   const client = await getClient();
   if (!isMongoClient(client)) return client;
 
-  // ============== Check if User is Authorized ==============================
-  const session = (await getServerSession()) as any;
-  if (!session) return NextResponse.json({ err: true, msg: "You are not authorized! This action will be noted." }, { status: 401 });
+  // ================== Validation ====================================
+  const errors = validateMultipleInputs(Object.values(data), Object.keys(data));
+  if (errors.length > 0) return NextResponse.json({ err: true, msg: errors }, { status: 422 });
 
-  // ============== Check if Old Password is Correct ==============================
-  if (passwordOld) {
-    const existingUser = (await getFromMongo(client, "users", { email: email }))[0] as User;
-    console.log("Compare old password: ", passwordOld, existingUser.password);
-    let passwordMatch = await verifyPassword(passwordOld, existingUser.password);
-    if (!passwordMatch) return NextResponse.json({ err: true, msg: "Incorrect old password for this email account." }, { status: 401 });
+  const continueData = trimObjectValues(data);
+
+  if (data.password) {
+    // ============== Check if Old Password is Correct ==============================
+    if (continueData.passwordOld) {
+      const existingUser = (await getFromMongo(client, "users", { email: continueData.email }))[0] as User;
+      let passwordMatch = await verifyPassword(continueData.passwordOld, existingUser.password);
+      if (!passwordMatch) return NextResponse.json({ err: true, msg: "Incorrect old password for this email account." }, { status: 401 });
+    }
   }
 
   // ================== Update User ===================================
   const date = new Date();
-  const newPassword = await hashPassword(password);
 
   try {
-    await updateInMongo(client, "users", { email: email }, { $set: { email: email, password: newPassword, updatedAt: date } });
+    if (data.password) {
+      const newPassword = await hashPassword(continueData.password);
+      await updateInMongo(client, "users", { email: continueData.email }, { $set: { email: continueData.email, password: newPassword, updatedAt: date } });
+    } else if (data.username) {
+      await updateInMongo(
+        client,
+        "users",
+        { email: continueData.email },
+        { $set: { email: continueData.email, username: continueData.username, updatedAt: date } }
+      );
+    }
   } catch (error) {
     return NextResponse.json({ err: true, msg: "Failed to Update Account. (2/2)" }, { status: 500 });
   }
