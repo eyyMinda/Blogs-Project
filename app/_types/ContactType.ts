8896db9f20e994634cf8595327e1db2d86ad53d0
eyyMinda <gamerx14x@gmail.com>
2023-12-17 23:34:55 +0200

    added: ContactFieldConfir interface

--- app/_types/ContactType.ts
+++ app/_types/ContactType.ts
@@ -1,5 +1,18 @@
-export interface ContactInfo extends DataObject {
+interface ContactInfo extends DataObject {
   name: string;
   email: string;
+  subject: string;
   message: string;
+}
+
+interface ContactFieldConfig {
+  name: "email" | "username" | "subject" | "message";
+  label?: string;
+  placeholder?: string;
+  type?: string;
+  description?: string;
+  rows?: number;
+  className?: string;
+  group?: boolean;
+  fields?: ContactFieldConfig[]; // For grouped fields
 }
