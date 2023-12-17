8cc8ecbb4bbd088a6c65abe471dadc6588398377
eyyMinda <gamerx14x@gmail.com>
2023-12-17 22:07:09 +0200

    refactor: form schema

--- lib/formSchema.ts
+++ lib/formSchema.ts
@@ -1,26 +1,48 @@
 import * as z from "zod";
 
-export const contactFormSchema = z.object({
-  username: z.string().min(4, { message: "Name must be at least 2 characters." }).max(24, { message: "Name exceeds the character limit." }),
-  email: z.string().min(10, { message: "Email must be at least 10 characters." }).max(40, { message: "Email exceeds the character limit." }).email(),
-  message: z.string().min(12, { message: "Should write a more informative message." }).max(1000, { message: "Message exceeds the character limit." })
-});
-
-// =================================================================
-
-const coreAuthSchema = {
+const REUSED = {
   email: z
     .string()
     .min(1, { message: "Enter an email." })
     .min(10, { message: "Email must be at least 10 characters." })
     .max(40, { message: "Email exceeds the character limit." })
     .email(),
+  username: z
+    .string()
+    .trim()
+    .min(1, { message: "Enter the username." })
+    .min(4, { message: "Username must be at least 4 characters long." })
+    .max(24, { message: "Username exceeds the character limit." }),
   password: z
     .string()
     .trim()
     .min(1, { message: "Enter a password." })
     .min(8, { message: "Password is too short." })
     .max(84, { message: "Password is too long." })
+};
+
+// ========================== CONTACT =======================================
+
+export const contactFormSchema = z.object({
+  username: REUSED.username,
+  email: REUSED.email,
+  subject: z
+    .string()
+    .min(1, { message: "Enter the subject." })
+    .min(2, { message: "Subject must be at least 2 characters." })
+    .max(200, { message: "Subject exceeds the character limit." }),
+  message: z
+    .string()
+    .min(1, { message: "Enter the message." })
+    .min(12, { message: "Should write a more informative message." })
+    .max(1000, { message: "Message exceeds the character limit." })
+});
+
+// =========================== AUTH ======================================
+
+const coreAuthSchema = {
+  email: REUSED.email,
+  password: REUSED.password
 };
 
 export const authFormSchema = (auth: "register" | "login") =>
@@ -31,15 +53,10 @@ export const authFormSchema = (auth: "register" | "login") =>
       .refine(data => data.password === data.passwordConfirm, { message: "Passwords do not match.", path: ["passwordConfirm"] })
   }[auth]);
 
-// =================================================================
+// =========================== CHANGE PASS ======================================
 
 const coreChangePassSchema = {
-  passwordNew: z
-    .string()
-    .trim()
-    .min(1, { message: "Enter new password." })
-    .min(8, { message: "New password is too short." })
-    .max(84, { message: "New password is too long." }),
+  passwordNew: REUSED.password,
   passwordConfirm: z.string().trim().min(1, { message: "Enter a matching password." })
 };
 
@@ -51,13 +68,6 @@ export const changePassFormSchema = (needPassword: boolean) => {
   return zObject.refine(data => data.passwordNew === data.passwordConfirm, { message: "Passwords do not match.", path: ["passwordConfirm"] });
 };
 
-// =================================================================
+// ============================ CHANGE USERNAME =====================================
 
-export const changeUsernameSchema = z.object({
-  username: z
-    .string()
-    .trim()
-    .min(1, { message: "Enter new username." })
-    .min(4, { message: "Username must be at least 4 characters." })
-    .max(24, { message: "Username exceeds the character limit." })
-});
+export const changeUsernameSchema = z.object({ username: REUSED.username });
