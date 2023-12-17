eb31d4559bb0c2e132e5b0857c14afe02fe9ef2b
eyyMinda <gamerx14x@gmail.com>
2023-12-17 23:42:22 +0200

    edit: header

--- components/contact/contact-card.tsx
+++ components/contact/contact-card.tsx
@@ -5,7 +5,7 @@ export default function ContactCard() {
   return (
     <Card className="w-11/12 sm:w-9/12 md:w-7/12 lg:w-6/12 2xl:w-8/12 overflow-hidden">
       <CardHeader className="bg-purple-900">
-        <h2 className="text-4xl mb-2 underline underline-offset-8">Contact    </h2>
+        <h2 className="text-4xl">Contact</h2>
       </CardHeader>
       <CardContent className="px-3 sm:px-6 pt-6">
         <ContactForm />
