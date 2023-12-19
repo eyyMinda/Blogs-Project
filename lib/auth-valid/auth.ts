53dd6c1aaae5d7e3a61bace46fe0b785abf5fb45
eyyMinda <gamerx14x@gmail.com>
2023-12-19 02:03:13 +0200

    edit: less digits randUsername

--- lib/auth-valid/auth.ts
+++ lib/auth-valid/auth.ts
@@ -4,4 +4,4 @@ export const hashPassword = async (password: string) => hash(password, 12);
 
 export const verifyPassword = async (password: string, hashedPassword: string) => compare(password, hashedPassword);
 
-export const newRandUsername = () => "user_" + Math.floor(Math.random() * 100000000000000);
+export const newRandUsername = () => "user_" + Math.floor(Math.random() * 1000000000000);
