97220852c79660c4d0913e58c379ae322c032e9f
Mindaugas Straksys <gamerx14x@gmail.com>
2024-07-29 18:34:18 +0300

    bump: multiple dependancies; installed emoji-picker; edited accordion tailwind animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,8 +82,8 @@ module.exports = {
         }
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out"
+        "accordion-down": "accordion-down 0.2s ease-out forwards",
+        "accordion-up": "accordion-up 0.2s ease-out forwards"
       }
     }
   },
