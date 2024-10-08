ccedf2381e7ce3a4daa8d911bb0f00449bfef4a6
Mindaugas Straksys <gamerx14x@gmail.com>
2024-10-08 21:48:50 +0300

    ui: reaction button adjustment

--- components/ui/custom-ui/reaction-btn.tsx
+++ components/ui/custom-ui/reaction-btn.tsx
@@ -1,16 +1,17 @@
+import { ReactNode } from "react";
 import { Button } from "@/components/ui/button";
 
 interface ReactionButtonParams {
   viewCount?: boolean;
   count: number;
   onClick: () => void;
-  icon: JSX.Element;
+  children: ReactNode;
 }
 
-const ReactionButton = ({ viewCount = false, count, onClick, icon }: ReactionButtonParams) => (
+const ReactionButton = ({ viewCount = false, count, onClick, children }: ReactionButtonParams) => (
   <div className="flex items-center">
     <Button variant="ghost" className="py-0 px-2 rounded-full" onClick={onClick}>
-      {icon}
+      {children}
     </Button>
     {viewCount && !!count && <span className="pt-1 text-sm text-gray-600 dark:text-gray-300">{count}</span>}
   </div>
