14eb6acc08c990773936da0c0961ba51cf20b557
Mindaugas Straksys <gamerx14x@gmail.com>
2024-10-08 21:42:38 +0300

    api: adjusted update-comment to be able to like replies

--- app/api/posts/update-comment/route.ts
+++ app/api/posts/update-comment/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: NextRequest) {
   if (!data) return NextResponse.json({ err: true, msg: "No data has been provided." }, { status: 400 });
 
   // ============= Validation =============================
-  const commentData = trimObjectValues(data, ["comment_id", "like"]);
+  const commentData = trimObjectValues(data.comment, ["comment_id", "like"]);
 
   // ============= Define/Redefine Client =============================
   const client = await getClient();
@@ -19,28 +19,43 @@ export async function POST(req: NextRequest) {
   const userID = existingUser._id.toString();
 
   // ============= Check if already liked/disliked =============================
-  const currentComment = (await getFromMongo(client, "comments", { _id: commentData.comment_id }))[0] as CommentType;
+  const currentComment = data.replyDepth
+    ? ((await getFromMongo(client, "comments", { "replies._id": commentData.comment_id }))[0] as CommentType)
+    : ((await getFromMongo(client, "comments", { _id: commentData.comment_id }))[0] as CommentType);
   if (!currentComment) return NextResponse.json({ err: true, msg: "This comment does not exist!" }, { status: 401 });
+
+  // ============= Extracting the reply for logic =============================
+  const replyComment = data.replyDepth ? (currentComment.replies?.find(obj => obj._id === commentData.comment_id) as CommentType) : currentComment;
 
   // Determine the action and update arrays accordingly
   const isLike: boolean = Boolean(commentData.like);
-  currentComment.likes = toggleStringInArray(currentComment.likes, userID, isLike && !currentComment.likes.includes(userID));
-  currentComment.dislikes = toggleStringInArray(currentComment.dislikes, userID, !isLike && !currentComment.dislikes.includes(userID));
+  replyComment.likes = toggleStringInArray(replyComment.likes, userID, isLike && !replyComment.likes.includes(userID));
+  replyComment.dislikes = toggleStringInArray(replyComment.dislikes, userID, !isLike && !replyComment.dislikes.includes(userID));
 
   // Log the action taken and set the status in one step
   let status: "liked" | "disliked" | "none" = "none";
   let message: string;
   if (isLike) {
-    status = currentComment.likes.includes(userID) ? "liked" : "none";
+    status = replyComment.likes.includes(userID) ? "liked" : "none";
     message = status === "liked" ? "User is liking the comment..." : "User has liked this comment already. Removing like...";
   } else {
-    status = currentComment.dislikes.includes(userID) ? "disliked" : "none";
+    status = replyComment.dislikes.includes(userID) ? "disliked" : "none";
     message = status === "disliked" ? "User is disliking the comment..." : "User has disliked this comment already. Removing dislike...";
   }
 
   // ============= Update Comment =============================
+  const updateQuery = data.replyDepth ? { "replies._id": commentData.comment_id } : { _id: commentData._id };
+  const updateOptions = data.replyDepth
+    ? {
+        $set: {
+          "replies.$.likes": replyComment.likes, // Positional operator ($) updates the specific element based on query
+          "replies.$.dislikes": replyComment.dislikes
+        }
+      }
+    : { $set: { likes: replyComment.likes, dislikes: replyComment.dislikes } };
+
   try {
-    await updateInMongo(client, "comments", { _id: currentComment._id }, { $set: { likes: currentComment.likes, dislikes: currentComment.dislikes } });
+    await updateInMongo(client, "comments", updateQuery, updateOptions);
   } catch (error) {
     console.log(error);
     return NextResponse.json({ err: true, msg: "Failed to update comment." }, { status: 500 });
@@ -48,8 +63,8 @@ export async function POST(req: NextRequest) {
   // ============= Final Reaction Response to Client =============================
   const returnData = {
     status,
-    likeCount: currentComment.likes.length,
-    dislikeCount: currentComment.dislikes.length
+    likeCount: replyComment.likes.length,
+    dislikeCount: replyComment.dislikes.length
   };
   return NextResponse.json({ err: false, returnData, msg: message }, { status: 200 });
 }
