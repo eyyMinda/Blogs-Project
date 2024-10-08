df177ffb36d14b40692303e59b4c7d128abb6731
Mindaugas Straksys <gamerx14x@gmail.com>
2024-10-08 21:43:46 +0300

    added: replies comp

---
+++ components/posts/comments/replies.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Dispatch, SetStateAction, useState } from "react";
+import CommentList from "./comment-list";
+import { ChevronDown, ChevronUp } from "lucide-react";
+
+export default function Replies({ replies, setNewCommentPosted }: { replies: CommentType[]; setNewCommentPosted: Dispatch<SetStateAction<boolean>> }) {
+  const [repliesOpen, setRepliesOpen] = useState<boolean>(false);
+  const repliesLength = replies.length;
+  return (
+    <>
+      <Button variant="ghost" onClick={() => setRepliesOpen(v => !v)} className="flex items-center p-2 h-auto pr-4 gap-1 text-red-500 rounded-3xl">
+        {repliesOpen ? <ChevronUp /> : <ChevronDown />}
+        {repliesLength} {repliesLength > 1 ? "replies" : "reply"}
+      </Button>
+
+      {repliesOpen && <CommentList comments={replies} replyDepth={true} className="ml-8" setNewCommentPosted={setNewCommentPosted} />}
+    </>
+  );
+}
