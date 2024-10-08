a93d758cc4dcebc0a0590b7aec3015d008ecf53f
Mindaugas Straksys <gamerx14x@gmail.com>
2024-10-08 21:47:26 +0300

    expanded: comment to function as a reply

--- components/posts/comments/comment.tsx
+++ components/posts/comments/comment.tsx
@@ -1,15 +1,30 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { timeAgo } from "@/lib/utils";
 import { UpdateComment } from "@/lib/actions";
-import { ThumbsDown, ThumbsUp } from "lucide-react";
+import { ThumbsDown, ThumbsUp, Trash2, TrashIcon } from "lucide-react";
 import ReactionButton from "@/components/ui/custom-ui/reaction-btn";
+import ReplyComment from "./reply-comment";
+import { Button } from "@/components/ui/button";
+import Replies from "./replies";
 
-export function Comment({ skeleton = false, comment }: { skeleton?: boolean; comment?: CommentType }) {
+export function Comment({
+  skeleton = false,
+  comment,
+  replyDepth = false,
+  post_id,
+  setNewCommentPosted
+}: {
+  skeleton?: boolean;
+  comment?: CommentType;
+  replyDepth?: boolean;
+  post_id?: number;
+  setNewCommentPosted: Dispatch<SetStateAction<boolean>>;
+}) {
   const { replace } = useRouter();
   const { data: session } = useSession();
   const { id: userId } = session?.user || {};
@@ -19,6 +34,7 @@ export function Comment({ skeleton = false, comment }: { skeleton?: boolean; com
     likes: comment?.likes?.length || 0,
     dislikes: comment?.dislikes?.length || 0
   });
+  const [replyOpen, setReplyOpen] = useState<boolean>(false);
 
   useEffect(() => {
     if (userId && comment) {
@@ -30,17 +46,20 @@ export function Comment({ skeleton = false, comment }: { skeleton?: boolean; com
     if (!session?.user) return replace("/login");
     if (!comment?._id) return;
     const { status, likeCount, dislikeCount }: ReactionUpdateType = await UpdateComment({
-      like,
-      comment_id: comment._id,
-      user: session.user.name as string,
-      email: session.user.email as string
+      comment: {
+        like,
+        comment_id: comment._id,
+        user: session.user.name as string,
+        email: session.user.email as string
+      },
+      replyDepth
     });
     setReactionState(status);
     setCounts({ likes: likeCount, dislikes: dislikeCount });
   };
 
   return (
-    <li className={`text-left ${skeleton ? "skeleton rounded-lg" : ""}`}>
+    <li className={`text-left relative ${skeleton ? "skeleton rounded-lg" : ""}`}>
       {!skeleton && comment && (
         <>
           <div className="flex gap-2 mb-1">
@@ -61,18 +80,32 @@ export function Comment({ skeleton = false, comment }: { skeleton?: boolean; com
 
       {!skeleton && (
         <div className="flex items-center gap-2">
-          <ReactionButton
-            viewCount
-            count={counts.likes}
-            onClick={() => handleCommentReaction(true)}
-            icon={<ThumbsUp strokeWidth={reactionState === "liked" ? 2.5 : 0.5} className="w-5 h-auto" />}
-          />
+          <ReactionButton viewCount count={counts.likes} onClick={() => handleCommentReaction(true)}>
+            {<ThumbsUp strokeWidth={reactionState === "liked" ? 2.5 : 0.5} className="w-5 h-auto" />}
+          </ReactionButton>
+          <ReactionButton count={counts.dislikes} onClick={() => handleCommentReaction(false)}>
+            {<ThumbsDown strokeWidth={reactionState === "disliked" ? 2.5 : 0.5} className="w-5 h-auto" />}
+          </ReactionButton>
 
-          <ReactionButton
-            count={counts.dislikes}
-            onClick={() => handleCommentReaction(false)}
-            icon={<ThumbsDown strokeWidth={reactionState === "disliked" ? 2.5 : 0.5} className="w-5 h-auto" />}
-          />
+          <Button variant="ghost" className="p-1 min-h-0 h-auto rounded-2xl" onClick={() => setReplyOpen(v => !v)}>
+            Reply
+          </Button>
+        </div>
+      )}
+      {replyOpen && (
+        <ReplyComment
+          setReplyOpen={setReplyOpen}
+          replyDepth={replyDepth}
+          authorUsername={comment?.username}
+          post_id={post_id!}
+          comment_id={comment?._id as string}
+          setNewCommentPosted={setNewCommentPosted}
+        />
+      )}
+      {comment?.replies && comment?.replies?.length > 0 && <Replies replies={comment.replies} setNewCommentPosted={setNewCommentPosted} />}
+      {!skeleton && (
+        <div className="absolute right-5 top-5 cursor-pointer text-white/50 transition-all hover:text-white">
+          <Trash2 size={18} strokeWidth={1.4} />
         </div>
       )}
     </li>
