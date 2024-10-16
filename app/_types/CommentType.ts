8242503c098c30741d6319c310879373f7eeb2a3
Mindaugas Straksys <gamerx14x@gmail.com>
2024-10-17 01:31:19 +0300

    type: Comment types with User aggregated data

--- app/_types/CommentType.ts
+++ app/_types/CommentType.ts
@@ -1,8 +1,7 @@
 interface CommentType {
   _id: string | number;
   post_id: string | number;
-  username: string;
-  email: string;
+  author_id: string;
   comment: string;
   date: string;
   replies?: CommentReplyType[];
@@ -10,10 +9,13 @@ interface CommentType {
   dislikes: string[];
 }
 
-interface CommentReplyType extends CommentType {}
+interface CommentWithUserType extends CommentType {
+  author: { name: string; email: string; image: string };
+}
+interface CommentReplyType extends CommentWithUserType {}
 
 interface CommentsType {
-  comments?: CommentType[] | undefined;
+  comments?: CommentType[] | CommentWithUserType[] | undefined;
 }
 
 type SortOption = "latest" | "oldest" | "popular";
@@ -28,8 +30,7 @@ interface ReactionUpdateType {
 interface CommentReactionUpdateParams {
   like: boolean; // boolean
   comment_id: string | number;
-  user: string;
-  email: string;
+  author_id: string;
 }
 
 interface CommentReplyUpdateParams extends CommentType {
@@ -39,12 +40,10 @@ interface CommentReplyUpdateParams extends CommentType {
 interface CommentEditParams {
   comment_id: string | number;
   comment: string;
-  user: string;
-  email: string;
+  author_id: string;
 }
 
 interface CommentDeleteParams {
   comment_id: string | number;
-  user: string;
-  email: string;
+  author_id: string;
 }
