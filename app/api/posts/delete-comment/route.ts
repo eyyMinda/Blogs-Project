bef89db8d5c24e8f07270e8822e88abd73b4b217
Mindaugas Straksys <gamerx14x@gmail.com>
2024-10-17 01:35:32 +0300

    api: new delete comment format for api

--- app/api/posts/delete-comment/route.ts
+++ app/api/posts/delete-comment/route.ts
@@ -1,4 +1,3 @@
-import { trimObjectValues } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 import { deleteFromMongo, getClient, getFromMongo, isMongoClient, updateInMongo } from "@/lib/mongo-db/mongo";
 
@@ -6,15 +5,13 @@ export async function POST(req: NextRequest) {
   const data = await req.json();
   if (!data) return NextResponse.json({ err: true, msg: "No data has been provided." }, { status: 400 });
 
-  // ============= Validation =============================
-  const comment = trimObjectValues(data.comment, ["comment_id"]);
-
   // ============= Define/Redefine Client =============================
   const client = await getClient();
   if (!isMongoClient(client)) return client;
 
   // ============= Check whether the user is authorised =============================
-  const existingUser = (await getFromMongo(client, "users", { email: data.comment.email }))[0] as User;
+  const comment = data.comment;
+  const existingUser = (await getFromMongo(client, "users", { $expr: { $eq: [{ $toString: "$_id" }, comment.author_id] } }))[0] as User;
   if (!existingUser) return NextResponse.json({ err: true, msg: "This user does not exist! You are not authorised for this action!" }, { status: 401 });
 
   // ============= Delete Comment =============================
