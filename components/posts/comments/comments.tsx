10ac3e98cd321378483cb2fdc525832b32eb5324
Mindaugas Straksys <gamerx14x@gmail.com>
2024-10-18 22:07:39 +0300

    replaced: client-side sorting with mongodb query and added view more btn for 'pagination'

--- components/posts/comments/comments.tsx
+++ components/posts/comments/comments.tsx
@@ -2,38 +2,41 @@
 
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+import { COMMENTS_PAGE_SIZE } from "@/lib/constants";
 import { Button } from "@/components/ui/button";
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
-import { fetchComments } from "@/lib/actions";
-import { sortComments } from "@/lib/utils";
 import SortCommentsButton from "./sort-comments-btn";
+import { fetchComments } from "@/lib/actions";
 
 export default function Comments({ post_id }: { post_id: number }) {
   const { data: session } = useSession();
   const [showComments, setShowComments] = useState(false);
-  const [fetchedComments, setFetchedComments] = useState<CommentWithUserType[] | null>(null);
   const [comments, setComments] = useState<CommentWithUserType[] | null>(null);
-  const [newCommentPosted, setNewCommentPosted] = useState(false);
+  const [totalCommentsCount, setTotalCommentsCount] = useState(0);
+  const [newCommentPosted, setNewCommentPosted] = useState(false); // Refetch comments if true;
   const [sortOption, setSortOption] = useState<SortOption>("latest");
+  const [pageSize, setPageSize] = useState(10);
+  const [viewMoreButton, setViewMoreButton] = useState(false);
 
   useEffect(() => {
     if (showComments) {
       const getComments = async () => {
-        const fetchedComments = await fetchComments({ post_id, skip: 0 });
-        setFetchedComments(fetchedComments);
+        const { comments: fetchedComments, metadata } = await fetchComments({ post_id, pageSize, sortOption });
+        console.log("Metadata: ", metadata);
+        setTotalCommentsCount(metadata.count);
+        setComments(fetchedComments);
         setNewCommentPosted(false);
+        setViewMoreButton(metadata.count > pageSize);
       };
       getComments();
     }
-  }, [post_id, showComments, newCommentPosted]);
+  }, [post_id, showComments, newCommentPosted, pageSize, sortOption]);
 
-  useEffect(() => {
-    if (fetchedComments) {
-      const sortedComments = sortComments(fetchedComments, sortOption);
-      setComments(sortedComments);
-    }
-  }, [fetchedComments, sortOption]);
+  const fetchMoreComments = async () => {
+    if (totalCommentsCount < pageSize) return;
+    setPageSize(v => v + COMMENTS_PAGE_SIZE);
+  };
 
   return (
     <div className="w-full flex flex-col justify-center">
@@ -44,14 +47,20 @@ export default function Comments({ post_id }: { post_id: number }) {
       {showComments && (
         <>
           <div className="flex justify-between mb-2">
-            <h2 className="font-bold text-2xl text-start mb-2">{comments?.length ? `${comments.length} Comments` : "0 Comments"}</h2>
+            <h2 className="font-bold text-2xl text-start mb-2">{`${totalCommentsCount} Comments`}</h2>
 
-            {fetchedComments && <SortCommentsButton setSortOption={setSortOption} />}
+            {!!totalCommentsCount && <SortCommentsButton setSortOption={setSortOption} />}
           </div>
 
           {session && <NewComment setNewCommentPosted={setNewCommentPosted} post_id={post_id} />}
 
           <CommentList comments={comments} post_id={post_id} setNewCommentPosted={setNewCommentPosted} />
+
+          {viewMoreButton && (
+            <Button variant="outline" onClick={fetchMoreComments} className="self-center rounded-3xl">
+              View more
+            </Button>
+          )}
         </>
       )}
     </div>
